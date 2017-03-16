f74d39d824e5ae23a6e2643062f6b10e596806be
dupontbertrand <dupontbertrand59@gmail.com>
2017-03-16 23:10:06 +0100

    add private tasks and allow only the creator to remove his own todo

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { Tasks } from '../api/tasks.js';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
-import './body.html';
+import { Tasks } from '../api/tasks.js';
+
 import './task.js';
+import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
@@ -18,12 +20,11 @@ Template.body.helpers({
       return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
     }
     // Otherwise, return all of the tasks
-    // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
   incompleteCount() {
-  return Tasks.find({ checked: { $ne: true } }).count();
-},
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
 
 Template.body.events({
@@ -36,12 +37,7 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
